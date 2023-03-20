8b6b90f6ff9b2c35e97e4359e1b5034a12e2bead
PiotrKoczkodaj <piotr.koczkodaj@vp.pl>
2023-03-20 20:28:55 +0100

    .

---
+++ src/fetchCountries.js
@@ -0,0 +1,9 @@
+import { input } from "./index";
+
+export const fetchCountries = (name) => {
+    name = `${input.value}`.trim();
+    // const countries = fetch('https://restcountries.com/v3.1/name/' + `${name}?fields=name,capital,population,flags,languages`);
+    const countries = fetch('https://restcountries.com/v3.1/name/'+`${name}`+'?fields=name,capital,population,flags,languages');
+    return countries;
+}
+
--- src/index.js
+++ src/index.js
@@ -1,3 +1,55 @@
 import './css/styles.css';
-
+import _ from 'lodash';
+import { fetchCountries } from './fetchCountries';
 const DEBOUNCE_DELAY = 300;
+export const input = document.querySelector('#search-box');
+
+const handler = _.debounce(() => {
+
+    fetchCountries(name).then((response) => {
+    
+        return response.json();
+    
+    }).then((response) => {
+        
+    if (name === '') {
+    input.removeEventListener('input',handler);
+}
+        response.map((res) => {
+        let nameOfficial = res.name.official;
+        let capitalsArray = res.capital;
+        let population = res.population;
+        let flags = res.flags.svg;
+        let language = Object.values(res.languages);
+
+            console.log(res);
+     })   
+    })
+}, DEBOUNCE_DELAY);
+
+input.addEventListener('input', handler)
+
+
+
+
+// const handler = () => {
+
+// }
+// input.addEventListener('input', _.debounce(() => {         fetchCountries(name).then((response) => {
+    
+//         return response.json();
+    
+//     }).then((response) => {
+//         // let nameOfficial = response[0].name.official;
+//         // let capital = response[0].capital[0];
+//         // let population = response[0].population;
+//         // let flags = response[0].flags.svg;
+//         // let languagesObj = response[0].languages;
+//         // let language = Object.values(languagesObj)[0];
+
+//         response.map((res) => {
+//      console.log(res.name.official)
+//  })
+       
+//     })},1000))
+
