3a4b6e36806ecaa39c6e7bb614927717343fe403
PiotrKoczkodaj <piotr.koczkodaj@vp.pl>
2023-03-22 23:09:19 +0100

    .

--- src/index.js
+++ src/index.js
@@ -1,33 +1,27 @@
 import './css/styles.css';
 import _ from 'lodash';
+import Notiflix from 'notiflix';
 import { fetchCountries } from './fetchCountries';
 const DEBOUNCE_DELAY = 300;
+
 export const input = document.querySelector('#search-box');
 const list = document.querySelector('.country-list');
 
 const handler = _.debounce(() => {
-
     fetchCountries(name).then((response) => {
-    
         return response.json();
-    
     }).then((response) => {
-        
-    if (name === '') {
-    input.removeEventListener('input',handler);
-        }
-    
+    if (response.length > 10) {
+            return Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
+             }
         response.map((res) => {
         let nameOfficial = res.name.official;
         let capitalsArray = res.capital;
         let population = res.population;
         let flags = res.flags.svg;
-            let language = Object.values(res.languages);
-            
-             if (response.length > 10) {
-            return alert('DOKLADNIEJ');
-             }
-            if (response.length < 2) {
+        let language = Object.values(res.languages);
+    
+            if (response.length === 1) {
                 list.innerHTML = `
                 <li>${nameOfficial}</li>
                 <li>
@@ -36,15 +30,14 @@ const handler = _.debounce(() => {
                 <li>${capitalsArray}</li>
                 <li>${population}</li>
                 <li>${language}</li>
-                `
-            }
-            list.innerHTML += `
+                `} else {
+                  list.innerHTML += `
             <li>${nameOfficial}</li>
             <li><img src="${flags}" width="60" height="70" ></li>
-            `
-
-            console.log(res);
+            `};
      })   
+    }).catch((error) => {
+        return Notiflix.Notify.failure('Oops, there is no country with that name')
     })
 }, DEBOUNCE_DELAY);
 
@@ -53,24 +46,5 @@ input.addEventListener('input', handler)
 
 
 
-// const handler = () => {
 
-// }
-// input.addEventListener('input', _.debounce(() => {         fetchCountries(name).then((response) => {
-    
-//         return response.json();
-    
-//     }).then((response) => {
-//         // let nameOfficial = response[0].name.official;
-//         // let capital = response[0].capital[0];
-//         // let population = response[0].population;
-//         // let flags = response[0].flags.svg;
-//         // let languagesObj = response[0].languages;
-//         // let language = Object.values(languagesObj)[0];
-
-//         response.map((res) => {
-//      console.log(res.name.official)
-//  })
-       
-//     })},1000))
 
